1a217f797875967409094a69e79066648e36708a
R-I-T-U <ritukhwalapala@gmail.com>
2024-10-30 19:48:21 +0545

    tryin to learn merge

--- p14.java
+++ p14.java
@@ -1,4 +1,9 @@
 //jagged array
+import java.util.*;
 public class p14 {
-    
+    public static void main(String args[]){
+        //trying to learn how to erge if confilic arises
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+    }
 }
